d51a75560b75efcd0e53befc05ec090e330e7765
w123 <w123@sss.(none)>
2018-10-26 15:53:20 +0800

    Js 下载文件

---
+++ test_my/shell_script/curl_hbase_rest.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#req = `curl -vi -X PUT -H "Accept:text/xml" -H "Content-Type:text/xml" -d @filter_xml http://192.168.11.133:9900/users/scanner`
+req = `curl www.baidu.com`
+echo $req
