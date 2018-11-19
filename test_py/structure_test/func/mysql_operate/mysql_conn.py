a54e43678991d57b78acca737a188b6e31c642dd
w123 <w123@sss.(none)>
2018-11-19 17:33:37 +0800

    链接数据库测试

--- test_py/structure_test/func/mysql_operate/mysql_conn.py
+++ test_py/structure_test/func/mysql_operate/mysql_conn.py
@@ -9,7 +9,10 @@ import pymysql
 '''
 
 class Mysql_conn:
-	
+	choose_obj = {
+		"show" : "self._show_sql",
+		"do" : "self._do_sql"
+	}
 	def __init__(self, config_data):
 		config_obj = {
 			"host":None,
@@ -28,16 +31,51 @@ class Mysql_conn:
 				config_arr.append(config_obj[key])
 				continue
 			config_arr.append(config_data[key])
-		print(config_arr)
+		logging.info(config_arr)
 		try:
-			self.Mydb = pymysql.Connect(*config_arr)
+			self.Conn = pymysql.Connect(*config_arr)
 		except Exception as e:
 			print("Mysql_conn 连接错误!", e)
 
 	def __del__(self):
 		logging.info("__del__")
-		self.Mydb.close()
+		self.Conn.close()
+	
+	def _create_sql(self, method, data_dict):
+		print("method:", method)
+		print("data_dict:", data_dict)
+		the_sql = "show databases"
+		row, res = exec(self.choose_obj[method] + '(the_sql)')
+		if not res:
+			print("sql do it error")
+	
+	def _show_sql(self, the_sql):
+		# 获取游标
+		res = True
+		cursor = self.Conn.cursor()
+		try:
+			cursor.execute(the_sql)
+		except Exception as e:
+			print("show_sql mysql error: ", e)
+			res = False
+		row = cursor.fetchall()
+		return row, res
 
-	def _do_choose(self, func):
-		print("choose func:", func)
-		logging.info("Mydb:", self.Mydb)
+	def _do_sql(self, the_sql):
+		# 获取游标
+		res = True
+		cursor = self.Conn.cursor()
+		try:
+			for sql, data in the_sql:
+				print("sql:", sql)
+				print("data", data)
+				cursor.execute(sql, data)
+		except Exception as e:
+			print("do_sql mysql error: ", e)
+			# 事务回滚
+			self.Conn.rollback()
+			res = False
+		else:
+			self.Conn.commit()
+		cursor.close()
+		return _, res
--- test_py/structure_test/mysql_operate.py
+++ test_py/structure_test/mysql_operate.py
@@ -4,11 +4,17 @@
 import os, sys
 import json
 import logging
-#logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.INFO)
 
 import conf.mysql_operate as Cmysql
 import func.mysql_operate as Mysql_obj
 #import func.mysql_operate.mysql_conn as Fsql_conn
+
+def create_data():
+	data_dict = {
+		"table":"tables",
+	}
+	return data_dict
 
 def main():
 	print("main")
@@ -17,10 +23,21 @@ def main():
 	logging.info(config)
 
 	#链接数据库
-#	print(Mysql_obj.mysql_conn)
 	mysql_conn = Mysql_obj.mysql_conn.Mysql_conn(config)
+
+	#操作数据库
+	data_dict = create_data()
+	logging.info(data_dict)
+	mysql_conn._create_sql("show", data_dict)
+
+def ceshi():
+	the_sql = [("show %s",("tables"))]
+	for (sql, data) in the_sql:
+		print("sql:", sql)
+		print("data", data)
 
 if __name__ == "__main__" :
 	print("START")
 	main()
+#	ceshi()
 	print("END")
